26bca02258e9447b3e0d758bf1657a404ea513ca
Fran <fran.fiorani@softwire.com>
2024-07-10 11:22:57 +0100

    Added button interaction, and iteration over the stations. Also added drop down list for departures and flex box to centre the components

--- src/components/Stations.tsx
+++ src/components/Stations.tsx
@@ -4,13 +4,27 @@ import Station from './Station';
 
 const Stations: React.FC = () => {
 
+    const handleOnClick = async() => {
+        // https://www.lner.co.uk/travel-information/travelling-now/live-train-times/depart/EDB/KGX/#LiveDepResults
+        alert(departureStationChoice + arrivalStationChoice);
+    };
+
     const stationURLs = {
         Paddington: 'url',
                             
     };
 
+    const stationList = [
+        { id: 'PDT', name: 'Paddington' },
+        { id: 'LST', name: 'Liverpool Street' },
+        { id: 'KCX', name: 'Kings Cross' },
+        { id: 'KTN', name: 'Kentish Town' },
+        { id: 'LBG', name: 'London Bridge' },
+    ];
+
     // let stationChoice: string;
-    const [stationChoice, setStationChoices] = useState();
+    const [departureStationChoice, setDepartureStationChoices] = useState('');
+    const [arrivalStationChoice, setArrivalStationChoices] = useState('');
 
     useEffect(() => {
         fetchStations()
@@ -19,20 +33,26 @@ const Stations: React.FC = () => {
             .finally(() => console.log('finally'));
     }, []);
 
-    console.log('hello world');
-
     return (
-        <div>
-            <label htmlFor = "stations">Select the station: </label>
+        <div className = "flex-container">
+            <div>
+                <label htmlFor = "departure stations">Select the departure station: </label>
 
-            <select value = { stationChoice } name = "stations" id = "stations">
-                <Station id = { 'PDT' } name = { 'Paddington' }/>
-                <Station id = { 'LST' } name = { 'Liverpool Street' }/>
-                <Station id = { 'KCX' } name = { 'Kings Cross' }/>
-                <Station id = { 'KTN' } name = { 'Kentish Town' }/>
-                <Station id = { 'LBG' } name = { 'London Bridge' }/>
-            </select>
-            <button type = "button" id = "submitStation">Submit</button>
+                <select value = { departureStationChoice } name = "stations" id = "departure-stations" onChange = { (e) => setDepartureStationChoices(e.target.value) }> 
+                    { stationList.map( (stationObj: {id: string; name: string}, index: number) => (<Station key = { index } id = { stationObj.id } name = { stationObj.name }/>)) }
+                </select>
+            </div>
+            <div>
+                <label htmlFor = "arrival stations"> Select the arrival station: </label>
+
+                <select value = { arrivalStationChoice } name = "stations" id = "arrival-stations" onChange = { (e) => setArrivalStationChoices(e.target.value) }> 
+                    { stationList.map( (stationObj: {id: string; name: string}) => (<Station key = { stationObj.id } id = { stationObj.id } name = { stationObj.name }/>)) }
+                </select>
+            </div>
+            <br />
+            <div>
+                <button type = "button" id = "submitStation" onClick = { handleOnClick }>Submit</button>
+            </div>
         </div>
     );
 };
